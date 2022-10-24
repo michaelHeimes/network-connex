dd684dae6e97b7dd9e61d7742c95e10b00f67d2c
Michael Heimes <michael@cairndigitalmedia.com>
2022-10-23 21:58:46 -0400

    dev

--- template-parts/modules/banner-slider.php
+++ template-parts/modules/banner-slider.php
@@ -15,18 +15,31 @@
 				$heading = $slide['heading'];
 				$text = $slide['text'];
 				$color_theme = $slide['color_theme'];
+				$button_link = $slide['button_link'];
 			?>
 			<div class="swiper-slide has-bg" data-theme="color-theme-<?php echo $color_theme;?>">
 				<div class="bg" style="background-image: url('<?php echo $background_image['url'];?>');"></div>
+				<div class="bg mask"></div>
 				<div class="grid-container relative">
 					<div class="inner grid-x grid-margin-x align-middle">
 						<div class="cell small-12 large-10 large-offset-1 text-center color-white">
 							<?php if( !empty($heading) ):?>
-								<h1 class="color-white"><?php echo $heading;?></h1>
+								<h1><?php echo $heading;?></h1>
 							<?php endif;?>
 							<?php if( !empty($text) ):?>
-								<p class="color-white"><?php echo $text;?></p>
+								<p class="color-blue"><?php echo $text;?></p>
 							<?php endif;?>
+							<?php 
+							$link = $button_link;
+							if( $link ): 
+								$link_url = $link['url'];
+								$link_title = $link['title'];
+								$link_target = $link['target'] ? $link['target'] : '_self';
+								?>
+							<div class="btn-wrap">
+								<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+							</div>
+							<?php endif; ?>
 						</div>
 					</div>
 				</div>
--- template-parts/modules/banner.php
+++ template-parts/modules/banner.php
@@ -1,20 +1,31 @@
 <?php
-	$alternative_title = get_sub_field('alternative_title');
+	$heading = get_sub_field('heading');
 	$background_image = get_sub_field('background_image');
 	$color_theme = get_sub_field('color_theme');
+	$button_link = get_sub_field('button_link');
 ?>
 <header class="banner default-banner has-bg" color-theme="color-theme-<?php echo $color_theme;?>">
 	<div class="bg" style="background-image: url('<?php echo $background_image['url'];?>');"></div>
 	<div class="bg mask"></div>
 	<div class="grid-container relative">
-		<div class="inner grid-x grid-padding-x align-middle align-center">
-			<div class="cell small-12 text-center">
-				<?php if( !empty($alternative_title) ):?>
-					<h1><?php echo $alternative_title;?></h1>
-					
-				<?php else:?>
-					<h1><?php the_title();?></h1>
+		<div class="inner grid-x grid-padding-x align-middle">
+			<div class="cell small-12 large-10 large-offset-1 text-center">
+				<?php if( !empty($heading) ):?>
+					<h1><?php echo $heading?></h1>
 				<?php endif;?>
+				
+				<?php 
+				$link = $button_link;
+				if( $link ): 
+					$link_url = $link['url'];
+					$link_title = $link['title'];
+					$link_target = $link['target'] ? $link['target'] : '_self';
+					?>
+				<div class="btn-wrap">
+					<a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+				</div>
+				<?php endif; ?>
+				
 			</div>
 		</div>
 	</div>
